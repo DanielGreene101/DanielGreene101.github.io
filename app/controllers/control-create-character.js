a1d7ce2d2cb1bc4d4fb71bdc6f23efa0a5f3f86e
DanielGreene101 <d.b.greene101@gmail.com>
2017-09-11 17:02:03 -0500

    next step

--- app/controllers/control-create-character.js
+++ app/controllers/control-create-character.js
@@ -2,24 +2,233 @@
 
 app.controller('CreateCharCtrl', function ($scope, $location, $http) {
 
+//Empty arrays for pushed items
+	$scope.yourRace = [];
+	$scope.yourClass = [];
+	$scope.yourSkills = [];
+	$scope.yourProficiencies = [];
+	$scope.yourFeats = [];
+	$scope.yourEquipment = [];
+	$scope.yourMagicSchools = [];
+	$scope.yourSpells = [];
+	$scope.yourNotes = '';
+
+//handle pushing clicked item
+	$scope.addRace = (item) => {
+		console.log("item", item, item.name);
+		$scope.yourRace.pop();
+		$scope.yourRace.push(item.name);
+
+		console.log("added to form");
+	};
+	$scope.addClass = (item) => {
+		console.log("item", item, item.name);
+		$scope.yourClass.push(item.name);
+		console.log("added to form");
+	};
+	$scope.addSkill = (item) => {
+		console.log("item", item, item.name);
+		$scope.yourSkills.push(item.name);
+		console.log("added to form");
+	};
+	$scope.addFeature = (item) => {
+		console.log("item", item, item.name);
+		$scope.yourFeats.push(item.name);
+		console.log("added to form");
+	};
+	$scope.addproficiency = (item) => {
+		console.log("item", item, item.name);
+		$scope.yourProficiencies.push(item.name);
+		console.log("added to form");
+	};
+	$scope.addEquipment = (item) => {
+		console.log("item", item, item.name);
+		$scope.yourEquipment.push(item.name);
+		console.log("added to form");
+	};
+	$scope.addMagicSchools = (item) => {
+		console.log("item", item, item.name);
+		$scope.yourMagicSchools.push(item.name);
+		console.log("added to form");
+	};
+	$scope.addSpell = (item) => {
+		console.log("item", item, item.name);
+		$scope.yourSpells.push(item.name);
+		console.log("added to form");
+	};
+
+//Empty arrays for api calls
+$scope.races = [];
+$scope.subraces = [];
+$scope.classes = [];
+$scope.subclasses = [];
+$scope.skills = [];
+$scope.proficiencies = [];
+$scope.features = [];
+$scope.equipment = [];
+$scope.magicSchools = [];
+$scope.spells = [];
+
+//api calls
+function callRaces(){
+	$http({ method : 'GET',
+		url : 'http://www.dnd5eapi.co/api/races/',})
+		.then(function(data){
+			$scope.races = data.data.results;
+		})
+		.catch(function(){
+			console.log("ERROR");
+		});
+
+}
+callRaces();
+
+
+function callSubRaces(){
+	$http({ method : 'GET',
+		url : 'http://www.dnd5eapi.co/api/subraces/'})
+		.then(function(data){
+			$scope.subraces = data.data.results;
+		})
+		.catch(function(){
+			console.log("ERROR");
+		});
+
+}
+callSubRaces();
 
 
 
 function callClasses(){
-	var classes = 'http://www.dnd5eapi.co/api/classes/';
-	console.log(classes);
-	$http.get({
-		URL: classes,
-		dataType: "JSON",
-		success: function(result){
-			console.log('result', result);
-			result.starting_proficiencies.forEach(function(element){
-				console.log('element', element.name);
-			$('#classList').append(element.name);
-			});
-		}
+	$http({ method : 'GET',
+		url : 'http://www.dnd5eapi.co/api/classes/'})
+		.then(function(data){
+			$scope.classes = data.data.results;
+		})
+		.catch(function(){
+			console.log("ERROR");
+		});
 
-	});
 }
 callClasses();
+
+
+
+function callSubClasses(){
+	$http({ method : 'GET',
+		url : 'http://www.dnd5eapi.co/api/subclasses/'})
+		.then(function(data){
+			$scope.subclasses = data.data.results;
+		})
+		.catch(function(){
+			console.log("ERROR");
+		});
+
+}
+callSubClasses();
+
+// function callLevels(){
+// 	$http({ method : 'GET',
+// 		url : 'http://www.dnd5eapi.co/api/{class name}/level/'})
+// 		.then(function(data){
+// 			$scope.races = data.data.results;
+// 		})
+// 		.catch(function(){
+// 			console.log("ERROR");
+// 		});
+
+// }
+// callLevels();
+
+function callSkills(){
+	$http({ method : 'GET',
+		url : 'http://www.dnd5eapi.co/api/skills/'})
+		.then(function(data){
+			$scope.skills = data.data.results;
+		})
+		.catch(function(){
+			console.log("ERROR");
+		});
+
+}
+callSkills();
+
+function callproficiencies(){
+	$http({ method : 'GET',
+		url : 'http://www.dnd5eapi.co/api/proficiencies/'})
+		.then(function(data){
+			$scope.proficiencies = data.data.results;
+		})
+		.catch(function(){
+			console.log("ERROR");
+		});
+
+}
+callproficiencies();
+
+
+function callFeatures(){
+	$http({ method : 'GET',
+		url : 'http://www.dnd5eapi.co/api/features/'})
+		.then(function(data){
+			$scope.features = data.data.results;
+		})
+		.catch(function(){
+			console.log("ERROR");
+		});
+
+}
+callFeatures();
+
+function callEquipment(){
+	$http({ method : 'GET',
+		url : 'http://www.dnd5eapi.co/api/equipment/'})
+		.then(function(data){
+			$scope.equipment = data.data.results;
+		})
+		.catch(function(){
+			console.log("ERROR");
+		});
+
+}
+callEquipment();
+
+function callmagicSchools(){
+	$http({ method : 'GET',
+		url : 'http://www.dnd5eapi.co/api/magic-schools/'})
+		.then(function(data){
+			$scope.magicSchools = data.data.results;
+		})
+		.catch(function(){
+			console.log("ERROR");
+		});
+}
+callmagicSchools();
+
+function callSpells(){
+	$http({ method : 'GET',
+		url : 'http://www.dnd5eapi.co/api/spells/'})
+		.then(function(data){
+			$scope.spells = data.data.results;
+		})
+		.catch(function(){
+			console.log("ERROR");
+		});
+}
+callSpells();
+
+//connect new char notes to form notes
+$scope.submitNotes = (notes) => {
+	console.log("hello");
+	console.log("notes", notes);
+	$scope.yourNotes = notes;
+
+
+};
+
+//submit new character
+
+
+
+
 });
